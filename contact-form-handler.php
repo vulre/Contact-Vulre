6616227a11d2ec26a299bd5b77513213a69b530c
vulre <vulreking@gmail.com>
2022-06-30 16:32:15 +0530

    contact me

---
+++ contact-form-handler.php
@@ -0,0 +1,22 @@
+<?php
+$name = $_POST['name'];
+$visitor_email = $_POST['email'];
+$message = $_POST['message'];
+
+$email_form = 'hdbkundi@gmail.com';
+
+$email_subject = "New Form Submission";
+$email_body = "User Name: $name.\n".
+                "User Email: $visitor_email.\n".
+                "User Message: $message.\n";
+
+                $to = "vulreking@gmail.com";
+
+                $headers = "From: $visitor_email \r\n";
+
+                mail($to,$email_subject,$email_body,$headers);
+
+                header("Location: index.html")
+
+
+                ?>
